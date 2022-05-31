fffd83cf2e19c352642aadd90f02de28d8b212e5
Jérôme FOULET <Gom3.78@Gmail.Com>
2022-05-31 15:51:22 +0200

    feat(test): add expect on viex/bills

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -26,6 +26,8 @@ describe("Given I am connected as an employee", () => {
       await waitFor(() => screen.getByTestId('icon-window'))
       const windowIcon = screen.getByTestId('icon-window')
       //to-do write expect expression
+      expect(windowIcon.classList.contains('active-icon')).toBe(true)
+      //expect(windowIcon.toHaveClass('active-icon')) // with jest-dom
 
     })
     test("Then bills should be ordered from earliest to latest", () => {
